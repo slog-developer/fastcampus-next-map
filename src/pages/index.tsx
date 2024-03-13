e961192b962f5128b89f98b095a98282fc3a69ba
seokgyeong <amisgyeong@gmail.com>
2024-03-13 15:07:53 +0900

    데이터 fetching 이해하기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,23 +4,31 @@ import Map from "@/components/Map";
 import Markers from "@/components/Markers";
 
 import * as stores from "@/data/store_data.json";
+import { StoreType } from "@/interface";
+
 import StoreBox from "@/components/StoreBox";
 
-export default function Home() {
+export default function Home({ stores }: { stores: StoreType[] }) {
   const [map, setMap] = useState(null);
   const [currentStore, setCurrentStore] = useState(null);
-  const storeDatas = stores["DATA"];
 
   console.log(currentStore);
   return (
     <>
       <Map setMap={setMap} />
-      <Markers
-        storeDatas={storeDatas}
-        map={map}
-        setCurrentStore={setCurrentStore}
-      />
+      <Markers stores={stores} map={map} setCurrentStore={setCurrentStore} />
       <StoreBox store={currentStore} setStore={setCurrentStore} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const stores = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/stores`
+  ).then((res) => res.json());
+
+  return {
+    props: { stores },
+    revalidate: 60 * 60,
+  };
 }
